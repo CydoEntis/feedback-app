113fe1c10cceea9b50dfc8f5d46ed95b6c68928b
Cody Stine <cydoentis@gmail.com>
2021-12-10 12:33:30 -0500

    Added Feedback stats

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
 import FEEDBACK_DATA from './data/FeedbackData';
 
 function App() {
   const [feedback, setFeedback] = useState(FEEDBACK_DATA);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
